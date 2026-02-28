3b85a8a61cdce28167ee659877338050b1b01adc
Mohamed Hamdy <mo.hamdy9520@gmail.com>
2026-02-28 07:15:33 +0200

    todo app final

--- lib/features/update_profile/update_profile_screen.dart
+++ lib/features/update_profile/update_profile_screen.dart
@@ -11,26 +11,35 @@ class UpdateProfileScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          CustomAppButtom(title: "Title",
-          onPressed: (){
-            showModalBottomSheet(context: context, builder: (context){
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  spacing: 20.h,
-                  children: [
-                    CustomAppButtom(title: "Upload from Camera",
-                    onPressed: (){},),
+          CircleAvatar(
+            radius:100.h,
+            child: Icon(Icons.person, size: 100.w),
+          ),
+          SizedBox(height: 20.h,),
 
-                    SizedBox(height: 10.h,),
-                    CustomAppButtom(title: "Upload from gallery",
-                    onPressed: (){},)
-                  ],
-                ),
-              );
-            });
-          },)
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: CustomAppButtom(title: "Change Profile Picture",
+            onPressed: (){
+              showModalBottomSheet(context: context, builder: (context){
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    spacing: 20.h,
+                    children: [
+                      CustomAppButtom(title: "Upload from Camera",
+                      onPressed: (){},),
+
+                      SizedBox(height: 10.h,),
+                      CustomAppButtom(title: "Upload from gallery",
+                      onPressed: (){},)
+                    ],
+                  ),
+                );
+              });
+            },),
+          )
         ],
       ),
     );
